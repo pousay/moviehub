739cedaa4816d57aa1c89343caf1feec1963c36a
pourya <pouryathesaymon@gmail.com>
2026-06-02 00:32:00 +0330

    content for comment added

--- backend/app/database/schema/comment.py
+++ backend/app/database/schema/comment.py
@@ -12,6 +12,7 @@ class Comment(Base):
     __tablename__ = "comments"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    content: Mapped[str] = mapped_column(nullable=False)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     media_id: Mapped[int] = mapped_column(ForeignKey("media.id"), nullable=False)
     reply_id: Mapped[Optional[int]] = mapped_column(
--- backend/app/models/comment.py
+++ backend/app/models/comment.py
@@ -1,15 +1,28 @@
-from pydantic import BaseModel
-from .user import UserModel
-from typing import Optional
+from pydantic import BaseModel, ConfigDict
+from typing import Optional, List
+
+
+class CommentCreateModel(BaseModel):
+    content: str
+    reply_id: Optional[int] = None
 
 
 class CommentModel(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
     id: int
     user_id: int
     media_id: int
-    reply_id: Optional[int]
-    parent_reply: Optional["CommentModel"]
-    user: UserModel
+    reply_id: Optional[int] = None
+    replies: Optional[List["CommentModel"]] = None
+
+
+class CommentUpdateModel(BaseModel):
+    content: str
+
+
+class CommentDeleteResponseModel(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
 
 
 CommentModel.model_rebuild()
