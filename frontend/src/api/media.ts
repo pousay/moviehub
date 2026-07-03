601e8d8916db26219a356b2292d43e0d521fe2ed
pourya <pouryathesaymon@gmail.com>
2026-07-03 18:30:32 +0330

    front added to

--- frontend/src/api/media.ts
+++ frontend/src/api/media.ts
@@ -2,10 +2,22 @@
 import { fetcher } from "./base";
 import type { Media } from "../types/media";
 
+export enum MediaFilter {
+  all = "all",
+  movie = "movie",
+  series = "series",
+}
+
 const MediaService = {
-  getRandom: (count = 4, minRate = 7) =>
+  getRandom: (count = 4, minRate = 7, filter: MediaFilter = MediaFilter.all) =>
     fetcher
-      .get<Media[]>("/media/random", { params: { count, min_rate: minRate } })
+      .get<Media[]>("/media/random", {
+        params: {
+          count,
+          min_rate: minRate,
+          media_type: filter,
+        },
+      })
       .then((r) => r.data),
 };
 
--- frontend/src/pages/Category.tsx
+++ frontend/src/pages/Category.tsx
@@ -2,43 +2,39 @@ import { useEffect, useState } from "react";
 import SuggestionsGrid from "../components/SuggestionGrid";
 import Sidebar from "../components/Sidebar";
 import Navbar from "../components/Navbar";
-import MediaService from "../api/media";
-import type { Media } from "../types/media";
+import MediaService, { MediaFilter } from "../api/media";
 import { useNavigate, useParams } from "react-router-dom";
+import type { Media } from "../types/media";
 
 export default function Category() {
   const [activeNav, setActiveNav] = useState(0);
   const [activeTab, setActiveTab] = useState(0);
 
+  const [media, setMedia] = useState<Media[]>([]);
+
   const { filter } = useParams();
   const navigate = useNavigate();
 
-  const [media, setMedia] = useState<Media[]>([]);
+  const mediaFilter =
+    filter === "movies"
+      ? MediaFilter.movie
+      : filter === "series"
+        ? MediaFilter.series
+        : MediaFilter.all;
 
   useEffect(() => {
-    switch (filter) {
-      case "all":
-        break;
-      case "movies":
-        break;
-      case "series":
-        break;
-      default:
-        navigate("/all/all");
-
-        break;
+    if (!["all", "movies", "series"].includes(filter ?? "")) {
+      navigate("/all/all", { replace: true });
+      return;
     }
+
     const fetchMedia = async () => {
-      try {
-        const response = await MediaService.getRandom(30, 8);
-        setMedia(response);
-      } catch (err) {
-        console.log(err);
-      }
+      const response = await MediaService.getRandom(30, 8, mediaFilter);
+      setMedia(response);
     };
 
     fetchMedia();
-  }, [filter]);
+  }, [mediaFilter, filter, navigate]);
 
   return (
     <>
@@ -48,21 +44,6 @@ export default function Category() {
 
       <main className="mt-17 min-h-[calc(100vh-68px)] w-full p-4 pb-20 sm:ml-18 sm:p-7 sm:pb-7">
         {media && <SuggestionsGrid only_list={true} media={media} />}
-        {/* <div className="grid grid-cols-1 items-start gap-6 lg:grid-cols-[280px_1fr]"> */}
-        {/* <aside
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              gap: 20,
-              position: "sticky",
-              top: 20,
-            }}
-            className="left-panel"
-          ></aside> */}
-
-        {/* <div> */}
-        {/* </div> */}
-        {/* </div> */}
       </main>
     </>
   );
